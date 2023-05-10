7275c1e382af3468d3dc4bd3b9748586dd08cdb2
Francesco (ExtraWeb) <f.stinco@extra-web.it>
2023-05-10 13:10:40 +0200

    - Fix problema permessi Android 13

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -1300,12 +1300,12 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
 
     public void onRequestPermissionResult(int requestCode, String[] permissions,
                                           int[] grantResults) throws JSONException {
-        for (int r : grantResults) {
-            if (r == PackageManager.PERMISSION_DENIED) {
-                this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, PERMISSION_DENIED_ERROR));
-                return;
-            }
-        }
+        // for (int r : grantResults) {
+        //     if (r == PackageManager.PERMISSION_DENIED) {
+        //         this.callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, PERMISSION_DENIED_ERROR));
+        //         return;
+        //     }
+        // }
         switch (requestCode) {
             case TAKE_PIC_SEC:
                 takePicture(this.destType, this.encodingType);
